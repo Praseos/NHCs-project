77dc710897b0db6832e0ae66d969991b8e0981fd
Praseos <mirota0511@outlook.com>
2024-05-22 23:39:57 +0200

    test emergency-fix branch

--- combine_csv.py
+++ combine_csv.py
@@ -7,4 +7,4 @@ def combine(add_list,output_name):
         combined_df = pd.concat([combined_df, input_csv]).drop_duplicates().reset_index(drop=True)
     combined_df.to_csv(f'{output_name}.csv', index=False)
 
-combine(['abcr_amine_1.csv','abcr_amine_2.csv','Doug_amine_1.csv','Doug_amine_2.csv','Merck_amine_1.csv','Merck_amine_2.csv','tci_amine_1.csv','tci_amine_2.csv'],'amine')
+combine(['abcr_amine_1.csv','abcr_amine_2.csv','Doug_amine_1.csv','Doug_amine_2.csv','Merck_amine_1.csv','Merck_amine_2.csv','tci_amine_1.csv','tci_amine_2.csv'],'amide')
